94fcfd9c554a1f4ebf6ffd9afa3165bf2a352afe
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-13 16:10:05 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function writeCards(names, gift) {
+    let customCard = [];
+    for (let i = 0; i < names.length; i++) {
+      customCard.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`);
+    }
+    return customCard;
+  }
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
